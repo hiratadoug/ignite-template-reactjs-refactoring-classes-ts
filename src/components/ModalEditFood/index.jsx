63c6ed51553210513b596636db5c1a57414fd2d9
Douglas Hirata de Moura <hirata.doug@gmail.com>
2022-07-21 23:35:04 -0300

    Initial commit

--- src/components/ModalEditFood/index.jsx
+++ src/components/ModalEditFood/index.jsx
@@ -1,4 +1,4 @@
-import { Component, createRef } from 'react';
+import { Component, createRef, useEffect } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
