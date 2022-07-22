0b1d3e29b357cc1125fe10883b37d2e6676ccb7a
Douglas Hirata de Moura <hirata.doug@gmail.com>
2022-07-21 23:30:40 -0300

    Initial commit

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,6 +7,7 @@ import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
 
+
 class Dashboard extends Component {
   constructor(props) {
     super(props);
